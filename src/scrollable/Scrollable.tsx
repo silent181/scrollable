0832857b701009c3deb9e769ef6da189ba7f1386
silent181 <silent181@163.com>
2023-07-13 11:35:36 +0800

    chore: case problem

--- src/scrollable/Scrollable.tsx
+++ src/scrollable/Scrollable.tsx
@@ -1,330 +1,226 @@
-import { MutableRefObject } from 'react';
+import {
+  useEffect,
+  useRef,
+  cloneElement,
+  forwardRef,
+  ForwardedRef,
+  useImperativeHandle,
+  useMemo,
+  CSSProperties,
+  useState,
+} from 'react';
+import './index.css';
+import { ScrollableInstance, ScrollableProps } from './type';
 
-import { BaseInfo, ScrollDirection, ScrollCallback, ScrollInfo, ControllerOptions } from './type';
+import { Controller } from './controller';
 
-import { scrollManager } from './manager';
-
-const raf = (cb: () => void) => {
-  if (typeof window.requestAnimationFrame === 'function') {
-    requestAnimationFrame(() => cb());
-  } else {
-    cb();
-  }
+const defaultScrollbarProps: ScrollableProps['scrollbar'] = {
+  size: 6,
+  margin: 2,
+  borderRadius: 20,
+  disableInteraction: false,
+  backgroundColor: '#999',
 };
 
-const px2Rem = (px: number) => {
-  const fontSize = document.documentElement.style.fontSize;
+const scrollbarId = 'J_scrollbar';
 
-  if (fontSize) {
-    return `${px / parseFloat(fontSize)}rem`;
-  }
-
-  return '0rem';
-};
-
-const rem2Px = (rem: number) => {
-  const fontSize = document.documentElement.style.fontSize;
-
-  if (fontSize) {
-    return rem * parseFloat(fontSize);
-  }
-
-  return 0;
-};
-
-export class Controller {
-  flexContainer: HTMLElement;
-  scrollbarThumb: HTMLElement;
-  scrollbarWrapper: HTMLElement;
-  info: BaseInfo;
-  direction: ScrollDirection;
-  forceUpdate: () => void;
-
-  onScroll?: MutableRefObject<ScrollCallback | undefined>;
-
-  private isTransition = false;
-  private containerScrolling = false;
-  private scrollbarScrolling = false;
-  private scrollValue = 0;
-  private lastScrollValue = 0;
-  private startPosition: ScrollInfo = { x: undefined, y: undefined };
-  private transitionTime: number;
-  private unit?: ControllerOptions['unit'];
-
-  constructor(options: ControllerOptions) {
-    this.flexContainer = options.flexContainer;
-    this.scrollbarWrapper = options.flexContainer.nextElementSibling as HTMLElement;
-    this.scrollbarThumb = options.scrollbarThumb;
-    this.direction = options.direction;
-    this.onScroll = options.onScrollRef;
-    this.transitionTime = options.transitionTime || 200;
-    this.forceUpdate = options.forceUpdate;
-    this.unit = options.unit || 'px';
-
-    this.info = this.getBaseInfo();
-    console.log(this.info, 'info');
-  }
-
-  public init = () => {
-    this.scrollbarThumb.style[this.info.scrollbarProp] = this.info.thumbLengthPercent;
-    this.flexContainer?.classList?.add('__flex_container__');
-    this.scrollbarWrapper.style.display = 'block';
+const InternalScrollable = (props: ScrollableProps, ref: ForwardedRef<ScrollableInstance>) => {
+  const { scrollbar, onScroll, direction, id, children, style } = props;
+  const { size, margin, disableInteraction, backgroundColor, imgSrc, borderRadius } = {
+    ...defaultScrollbarProps,
+    ...scrollbar,
   };
 
-  public setNoScroll = () => {
-    this.flexContainer?.classList?.remove('__flex_container__');
-    this.scrollbarWrapper.style.display = 'none';
-    this.info.flexItems.forEach((el) => this.removeProperty(el, 'transform'));
-  };
+  const [_, forceUpdate] = useState(1);
 
-  public handleContainerStart = (e: any) => {
-    this.startScroll(e, 'container');
-  };
+  const containerRef = useRef<HTMLDivElement>(null);
+  const flexContainerRef = useRef<HTMLDivElement>(null);
+  const scrollbarRef = useRef<HTMLDivElement>(null);
+  const thumbRef = useRef<HTMLDivElement>(null);
+  const controllerRef = useRef<Controller>();
 
-  public handleScrollbarStart = (e: any) => {
-    this.startScroll(e, 'scrollbarThumb');
-    this.scrollbarThumb.style.opacity = '1';
-  };
+  const onScrollRef = useRef(onScroll);
 
-  public handleMove = (e: any) => {
-    if (!this.containerScrolling) {
-      return;
-    }
-
-    const diff = this.getEventPosition(e) - this.getStartPosition();
-
-    const value = this.lastScrollValue + diff;
-
-    this.doScroll(value);
-  };
-
-  public handleScrollbarMove = (e: any) => {
-    if (!this.scrollbarScrolling) {
-      return;
-    }
-
-    const diff = this.getEventPosition(e) - this.getStartPosition();
-
-    this.doScrollbarScroll(diff);
-  };
-
-  public handleEnd = () => {
-    this.scrollbarScrolling = false;
-    this.containerScrolling = false;
-    this.removeProperty(this.scrollbarThumb, 'opacity');
-  };
-
-  public handleWheel = (e: WheelEvent) => {
-    if (this.containerScrolling || this.scrollbarScrolling) {
-      return;
-    }
-
-    const nextScrollValue = this.scrollValue - e.deltaY;
-    this.doScroll(nextScrollValue, true);
-  };
-
-  public scroll = (relativeValue: number) => {
-    if (this.isTransition || this.info.noScroll) {
-      return;
-    }
-
-    const computedValue = this.scrollValue - relativeValue;
-
-    this.startTransition();
-    this.doScroll(computedValue, true);
-    this.endTransition();
-  };
-
-  public register = (key: string) => {
-    scrollManager.register(key, this);
-  };
-
-  public unregister = (key: string) => {
-    scrollManager.unregister(key);
-  };
-
-  private getBaseInfo = () => {
-    const getItemLength = (item: HTMLElement) => {
-      const { width, height } = item.getBoundingClientRect();
-
-      const calcMargin = (item: HTMLElement) => {
-        const getValueByProperty = (propVal: string) => {
-          const numberVal = parseFloat(propVal);
-
-          if (propVal.includes('rem')) {
-            return rem2Px(numberVal);
-          }
-
-          return numberVal;
-        };
-
-        const style = window.getComputedStyle(item);
-
-        const top = (style.marginTop || style['margin-top' as keyof CSSStyleDeclaration]) as string;
-        const right = (style.marginRight || style['margin-right' as keyof CSSStyleDeclaration]) as string;
-        const bottom = (style.marginBottom || style['margin-bottom' as keyof CSSStyleDeclaration]) as string;
-        const left = (style.marginLeft || style['margin-left' as keyof CSSStyleDeclaration]) as string;
-
-        return {
-          top: getValueByProperty(top),
-          right: getValueByProperty(right),
-          bottom: getValueByProperty(bottom),
-          left: getValueByProperty(left),
-        };
-      };
-
-      const margins = calcMargin(item);
-
-      if (this.direction === 'x') {
-        return width + margins.left + margins.right;
-      }
-
-      return height + margins.top + margins.bottom;
-    };
-
-    const flexItems = (this.flexContainer?.children ? Array.from(this.flexContainer.children) : []) as HTMLElement[];
-    const totalLength = flexItems.reduce((sum, cur) => sum + getItemLength(cur), 0);
-    const scrollbarProp = this.direction === 'x' ? 'width' : 'height';
-    const containerLength = this.flexContainer.getBoundingClientRect()[scrollbarProp];
-    const scrollLength = totalLength - containerLength;
-    const thumbLength = containerLength * (containerLength / totalLength);
-    const thumbLengthPercent = `${(100 * containerLength) / totalLength}%`;
-    const thumbScrollLength = containerLength - thumbLength;
-    const thumbScrollRatio = thumbScrollLength / scrollLength;
-    const noScroll = totalLength <= containerLength;
-
+  const methods = useMemo(() => {
     return {
-      flexItems,
-      scrollbarProp,
-      totalLength,
-      containerLength,
-      scrollLength,
-      thumbLength,
-      thumbScrollLength,
-      thumbScrollRatio,
-      thumbLengthPercent,
-      noScroll,
-    } as BaseInfo;
-  };
+      getContainer: () => containerRef.current,
+      getFlexContainer: () => flexContainerRef.current,
+      getController: () => controllerRef.current,
+      getScrollbar: () => containerRef.current!.querySelector(`#${scrollbarId}`),
+      scroll: (len: number) => {
+        controllerRef.current?.scroll(len);
+      },
+    };
+  }, []);
 
-  private getStartPosition = () => {
-    const p = this.startPosition;
+  useEffect(() => {
+    const flexContainerEl = flexContainerRef.current!;
+    const thumbEl = thumbRef.current!;
 
-    if (this.direction === 'x') {
-      return p.x!;
-    }
+    const update = () => forceUpdate((c) => c + 1);
 
-    return p.y!;
-  };
-
-  private removeProperty = (el: HTMLElement, prop: string) => {
-    if (typeof el.style.removeProperty === 'function') {
-      el.style.removeProperty(prop);
-    } else {
-      el.style[prop as any] = '';
-    }
-  };
-
-  private setStartPosition = (value: number) => {
-    if (this.direction === 'x') {
-      this.startPosition.x = value;
-    } else {
-      this.startPosition.y = value;
-    }
-  };
-
-  private getEventPosition = (e: MouseEvent | TouchEvent) => {
-    const prop = this.direction === 'x' ? 'clientX' : 'clientY';
-
-    if ((e as MouseEvent)[prop]) {
-      return (e as MouseEvent)[prop];
-    }
-
-    return (e as TouchEvent).touches[0][prop];
-  };
-
-  private startScroll = (e: any, type: 'container' | 'scrollbarThumb') => {
-    if (type === 'container') {
-      this.containerScrolling = true;
-    } else {
-      this.scrollbarScrolling = true;
-    }
-
-    this.setStartPosition(this.getEventPosition(e));
-    this.lastScrollValue = this.scrollValue;
-  };
-
-  private startTransition = () => {
-    this.isTransition = true;
-    this.info.flexItems.forEach((el) => {
-      el.style.transition = `transform ${this.transitionTime / 1000}s`;
+    const controller = new Controller({
+      flexContainer: flexContainerEl,
+      scrollbarThumb: thumbEl,
+      direction,
+      onScrollRef,
+      forceUpdate: update,
     });
-    this.scrollbarThumb.style.transition = `${this.transitionTime / 1000}s`;
-  };
 
-  private endTransition = () => {
-    setTimeout(() => {
-      this.removeProperty(this.scrollbarThumb, 'transition');
-      this.info.flexItems.forEach((el) => {
-        this.removeProperty(el, 'transition');
-      });
-      this.isTransition = false;
-    }, this.transitionTime + 100);
-  };
+    controllerRef.current = controller;
 
-  private moveItems = (scrollValue: number) => {
-    this.info.flexItems.forEach((el) => {
-      raf(() => {
-        el.style.transform = `translate${this.direction.toUpperCase()}(${this.getScrollValueStr(scrollValue)})`;
-      });
-    });
-  };
-
-  private moveScrollbar = (scrollValue: number) => {
-    const ratio = this.info.thumbScrollRatio;
-
-    raf(() => {
-      this.scrollbarThumb.style.transform = `translate${this.direction.toUpperCase()}(${this.getScrollValueStr(
-        -scrollValue * ratio
-      )})`;
-    });
-  };
-
-  private doScroll = (value: number, once = false) => {
-    if (value > 0 || Math.abs(value) > this.info.scrollLength) {
-      if (once) {
-        value = value > 0 ? 0 : -this.info.scrollLength;
-      } else {
+    const handleStart = (e: any) => {
+      // wheel click down
+      if (e.button === 1) {
         return;
       }
+
+      controller.handleContainerStart(e);
+    };
+
+    const handleScrollbarStart = (e: any) => {
+      if (disableInteraction) {
+        return;
+      }
+
+      controller.handleScrollbarStart(e);
+    };
+
+    const handleMove = (e: any) => {
+      controller.handleMove(e);
+      controller.handleScrollbarMove(e);
+    };
+
+    const handleEnd = () => {
+      controller.handleEnd();
+    };
+
+    const handleWheel = (e: any) => {
+      controller.handleWheel(e);
+      e.stopPropagation();
+    };
+
+    flexContainerEl.addEventListener('mousedown', handleStart);
+    flexContainerEl.addEventListener('touchstart', handleStart);
+    document.addEventListener('mousemove', handleMove);
+    document.addEventListener('touchmove', handleMove);
+    document.addEventListener('mouseup', handleEnd);
+    document.addEventListener('touchend', handleEnd);
+    thumbEl.addEventListener('mousedown', handleScrollbarStart);
+    thumbEl.addEventListener('touchstart', handleScrollbarStart);
+
+    if (direction === 'y') {
+      flexContainerEl.addEventListener('wheel', handleWheel);
     }
 
-    this.scrollValue = value;
+    let observer: MutationObserver | null = null;
 
-    this.moveItems(value);
-    this.moveScrollbar(value);
-
-    this.onScroll?.current?.({
-      x: this.direction === 'x' ? value : undefined,
-      y: this.direction === 'y' ? value : undefined,
-    });
-  };
-
-  private doScrollbarScroll = (diff: number) => {
-    if ((diff > 0 && this.scrollValue === this.info.scrollLength) || (diff < 0 && this.scrollValue === 0)) {
-      return;
+    if (typeof MutationObserver !== 'undefined') {
+      observer = new MutationObserver(update);
+      observer.observe(flexContainerEl, {
+        childList: true,
+        subtree: true,
+      });
+    } else {
+      console.warn('your env does not support MutationObserver');
     }
 
-    const computedValue = this.lastScrollValue + -diff / this.info.thumbScrollRatio;
+    const clear = (eventOnly = false) => {
+      flexContainerEl.removeEventListener('mousedown', handleStart);
+      flexContainerEl.removeEventListener('touchstart', handleStart);
+      document.removeEventListener('mousemove', handleMove);
+      document.removeEventListener('touchmove', handleMove);
+      document.removeEventListener('mouseup', handleEnd);
+      document.removeEventListener('touchend', handleEnd);
+      thumbEl.removeEventListener('mousedown', handleScrollbarStart);
+      thumbEl.removeEventListener('touchstart', handleScrollbarStart);
+      flexContainerEl.removeEventListener('wheel', handleWheel);
 
-    this.doScroll(computedValue);
-  };
+      if (!eventOnly) {
+        controller.unregister(id);
+        observer && observer.disconnect();
+      }
+    };
 
-  private getScrollValueStr = (scrollValue: number) => {
-    if (this.unit === 'rem') {
-      return `${px2Rem(scrollValue)}rem`;
+    controller.register(id);
+
+    if (controller.info.noScroll) {
+      controller.setNoScroll();
+      clear(true);
+    } else {
+      controller.init();
     }
 
-    return `${scrollValue}px`;
+    return () => clear();
+  });
+
+  useImperativeHandle(ref, () => methods);
+
+  const getBarStyle = () => {
+    let styles: CSSProperties = {};
+
+    if (direction === 'x') {
+      styles = {
+        width: '100%',
+        height: size,
+        bottom: margin,
+      };
+    }
+
+    if (direction === 'y') {
+      styles = {
+        width: size,
+        height: '100%',
+        right: margin,
+        top: 0,
+      };
+    }
+
+    return styles;
   };
-}
+
+  const getThumbStyle = () => {
+    let styles: CSSProperties = {
+      backgroundColor,
+      borderRadius,
+      cursor: disableInteraction ? undefined : 'pointer',
+    };
+
+    if (direction === 'x') {
+      styles = {
+        ...styles,
+        height: '100%',
+      };
+    }
+
+    if (direction === 'y') {
+      styles = {
+        ...styles,
+        width: '100%',
+      };
+    }
+
+    return styles;
+  };
+
+  return (
+    <div
+      className="scrollable-wrapper"
+      ref={containerRef}
+      onDragStart={(e) => e.preventDefault()}
+      data-scroll-id={id}
+      style={style}
+    >
+      {cloneElement(children, { ref: flexContainerRef })}
+      <div id={scrollbarId} className="scrollable-scrollbar" ref={scrollbarRef} style={getBarStyle()}>
+        <div className="scrollable-scrollbar-thumb" ref={thumbRef} style={getThumbStyle()}>
+          {imgSrc ? <img className="scrollable-scrollbar-img" src={imgSrc} /> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Scrollable = forwardRef(InternalScrollable);
+
+export default Scrollable;
